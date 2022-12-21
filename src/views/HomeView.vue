c92ac55321f2ce2dfef5a67794427aa2d7ef3af8
Emmanuel Sackey <aximilli1212@gmail.com>
2022-12-21 01:13:34 +0000

    Display set of lists in Home component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,29 @@
 <template>
   <div class="home">
+    <ItemsListComponent :items="items" />
   </div>
 </template>
 
 <script lang="ts">
+import {defineComponent} from "vue";
+import ItemsListComponent from "@/components/items/ItemsList.component.vue"
+export default defineComponent({
+  name:'Home',
+
+  components:{
+    ItemsListComponent
+  },
+  setup(){
+    const items:any[] = [
+      {id:1, name:"Jerusalema"},
+      {id:2, name:"Ikayayami"},
+      {id:3, name:"londonoze"},
+      {id:4, name:"Nanishielana"},
+    ]
+
+    return {
+        items
+    }
+  }
+})
 </script>
